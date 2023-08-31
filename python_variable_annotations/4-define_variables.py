5e8219d9f3cfca301935e0a4c99c27260b540010
micaelapicco <micaelaabril421@gmail.com>
2023-08-31 12:23:41 -0700

    task 4

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Task 4:
+
+Define and annotate the following variables with the specified values:
+
+a, an integer with a value of 1
+pi, a float with a value of 3.14
+i_understand_annotations, a boolean with a value of True
+school, a string with a value of “Holberton”
+"""
+
+
+a = 1
+pi = 3.14
+i_understand_annotations = True
+school = "Holberton"
