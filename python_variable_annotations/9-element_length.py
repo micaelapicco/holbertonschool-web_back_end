59a66d0cefe2470522f1a19c1e6ce480a7c64e0f
micaelapicco <micaelaabril421@gmail.com>
2023-08-31 16:04:30 -0700

    task 9

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -3,9 +3,9 @@
 Task 9:
 return values with the appropriate types
 """
-from typing import Tuple, Sequence, Iterable
+from typing import Tuple, Sequence, Iterable, List
 
 
-def element_length(lst: Iterable[Sequence]) -> Tuple[Sequence, int]:
-    """return a tuples"""
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """element lenght func"""
     return [(i, len(i)) for i in lst]
