4eaf19c9a86fc639bed0f3e645c977d7cc56d27a
micaelapicco <micaelaabril421@gmail.com>
2023-08-31 16:27:53 -0700

    fix variable

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -10,8 +10,8 @@ import random
 import asyncio
 
 
-async def wait_random(max_deley: int = 10) -> float:
+async def wait_random(max_delay: int = 10) -> float:
     """wait a random deley"""
-    deley = random.uniform(0, max_deley)
-    await asyncio.sleep(deley)
-    return deley
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
