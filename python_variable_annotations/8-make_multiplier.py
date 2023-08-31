6f313fe649721b812944abcdaf33d379e95da636
micaelapicco <micaelaabril421@gmail.com>
2023-08-31 15:59:09 -0700

    task 8

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -2,7 +2,8 @@
 """
 Task 8:
 Write a type-annotated function make_multiplier that takes a float
-multiplier as argument and returns a function that multiplies a float by multiplier.
+multiplier as argument and returns a function that multiplies a
+float by multiplier.
 """
 from typing import Callable
 
