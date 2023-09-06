ff8850cd4709e0876a365270d0e332afd8987787
micaelapicco <micaelaabril421@gmail.com>
2023-09-06 09:43:23 -0700

    task 9 - 11 second version

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -9,7 +9,7 @@ returns the list of school having a specific topic
 def schools_by_topic(mongo_collection, topic):
     """returns the list of school having a specific topic"""
     topic_list = []
-    topics = mongo_collection.find({"topic": topic})
+    topics = mongo_collection.find({"topics": topic})
     for topic in topics:
         topic_list.append(topic)
     return topic_list
--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -7,5 +7,5 @@ inserts a new document in a collection based on kwargs
 
 def insert_school(mongo_collection, **kwargs):
     """inserts a new document in a collection based on kwargs"""
-    mongo_collection.insert_one(kwargs)
+    mongo_collection.insert(kwargs)
     return mongo_collection.inserted_id
