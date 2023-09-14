c3b4a96514913a5314b372fbfcea3a9f1e2955fb
micaelapicco <micaelaabril421@gmail.com>
2023-09-14 08:14:34 -0700

    task 9

---
+++ ES6_basic/9-getFullBudget.js
@@ -0,0 +1,19 @@
+//  Task 9: Rewrite getFullBudgetObject to use ES6 method
+// properties in the fullBudget object
+
+import getBudgetObject from './7-getBudgetObject';
+
+export default function getFullBudgetObject(income, gdp, capita) {
+  const budget = getBudgetObject(income, gdp, capita);
+  const fullBudget = {
+    ...budget,
+    getIncomeInDollars(income) {
+      return `$${income}`;
+    },
+    getIncomeInEuros(income) {
+      return `${income} euros`;
+    },
+  };
+
+  return fullBudget;
+}
