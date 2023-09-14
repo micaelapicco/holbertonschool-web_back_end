11512da9046e414f7659fae7c99e9447dbef6a75
micaelapicco <micaelaabril421@gmail.com>
2023-09-14 07:31:40 -0700

    task 5

---
+++ ES6_basic/5-spread-operator.js
@@ -0,0 +1,6 @@
+// Task 5: Using spread syntax, concatenate 2 arrays and each
+// character of a string by modifying the function below.
+
+export default function concatArrays(array1, array2, string) {
+  return [...array1, ...array2, ...string];
+}
