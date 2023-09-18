79b3bffb67214d7632beb41f560689d2718cf309
micaelapicco <micaelaabril421@gmail.com>
2023-09-18 06:06:12 -0700

    task 2

---
+++ ES6_classes/2-hbtn_course.js
@@ -0,0 +1,48 @@
+// Implement a class named HolbertonCourse:
+
+// Make sure to verify the type of attributes during object creation
+// Each attribute must be stored in an “underscore” attribute version (ex: name is stored in _name)
+// Implement a getter and setter for each attribute.
+
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    if (!Array.isArray(students)) {
+      throw new TypeError('Students must be an array');
+    }
+
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set name(newName) {
+    this._name = newName;
+  }
+
+  set length(newlength) {
+    this._length = newlength;
+  }
+
+  set students(newstudents) {
+    this._students = newstudents;
+  }
+}
