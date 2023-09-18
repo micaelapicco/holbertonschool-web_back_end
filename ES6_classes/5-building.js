96306865cdbceff89964b6cce79386d0e0d0e37e
micaelapicco <micaelaabril421@gmail.com>
2023-09-18 07:23:32 -0700

    task 5

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,19 @@
+// Task 5: implement a class named building
+
+export default class Building {
+  constructor(sqft) {
+    this._sqft = sqft;
+
+    if (this.constructor !== Building && !this.evacuationWarningMessage) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(newSqft) {
+    this._sqft = newSqft;
+  }
+}
