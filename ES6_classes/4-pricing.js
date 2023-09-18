376c6cb5e335fce9c6b692e3a82631be7c11a940
micaelapicco <micaelaabril421@gmail.com>
2023-09-18 06:52:14 -0700

    task 4

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,31 @@
+// Task 4: implement a class named Pricing
+
+/*eslint-disable*/
+import Currency from './3-currency';
+/* eslint-enable */
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set amount(newAmount) {
+    this._amount = newAmount;
+  }
+
+  set currency(newCurrency) {
+    this._currency = newCurrency;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this.currency._name} (${this.currency._code})`;
+  }
+}
