d99c56b3f35fbb4252b96fa3fc0b2b18116fb7cb
micaelapicco <micaelaabril421@gmail.com>
2023-09-18 09:40:16 -0700

    update task 2 and 4

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -6,17 +6,6 @@
 
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    if (typeof name !== 'string') {
-      throw new TypeError('Name must be a string');
-    }
-
-    if (typeof length !== 'number') {
-      throw new TypeError('Length must be a number');
-    }
-    if (!Array.isArray(students)) {
-      throw new TypeError('Students must be an array');
-    }
-
     this._name = name;
     this._length = length;
     this._students = students;
@@ -35,14 +24,23 @@ export default class HolbertonCourse {
   }
 
   set name(newName) {
+    if (typeof newName !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
     this._name = newName;
   }
 
   set length(newlength) {
+    if (typeof newlength !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
     this._length = newlength;
   }
 
-  set students(newstudents) {
-    this._students = newstudents;
+  set students(newStudents) {
+    if (!Array.isArray(newStudents)) {
+      throw new TypeError('Students must be an array');
+    }
+    this._students = newStudents;
   }
 }
--- ES6_classes/4-pricing.js
+++ ES6_classes/4-pricing.js
@@ -28,4 +28,8 @@ export default class Pricing {
   displayFullPrice() {
     return `${this._amount} ${this.currency._name} (${this.currency._code})`;
   }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
 }
