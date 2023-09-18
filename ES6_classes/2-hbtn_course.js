bac2fc6b94cf4f778f1fa692dce811d1876b1f15
micaelapicco <micaelaabril421@gmail.com>
2023-09-18 10:12:45 -0700

    update task 2 and 7

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -5,42 +5,42 @@
 // Implement a getter and setter for each attribute.
 
 export default class HolbertonCourse {
-    constructor(name, length, students) {
-        this.name = name;
-        this.length = length;
-        this.students = students;
-    }
+  constructor(name, length, students) {
+    this.name = name;
+    this.length = length;
+    this.students = students;
+  }
 
-    get name() {
-        return this._name;
-    }
+  get name() {
+    return this._name;
+  }
 
-    get length() {
-        return this._length;
-    }
+  get length() {
+    return this._length;
+  }
 
-    get students() {
-        return this._students;
-    }
+  get students() {
+    return this._students;
+  }
 
-    set name(newName) {
-        if (typeof newName !== 'string') {
-            throw new TypeError('Name must be a string');
-        }
-        this._name = newName;
+  set name(newName) {
+    if (typeof newName !== 'string') {
+      throw new TypeError('Name must be a string');
     }
+    this._name = newName;
+  }
 
-    set length(newlength) {
-        if (typeof newlength !== 'number') {
-            throw new TypeError('Length must be a number');
-        }
-        this._length = newlength;
+  set length(newlength) {
+    if (typeof newlength !== 'number') {
+      throw new TypeError('Length must be a number');
     }
+    this._length = newlength;
+  }
 
-    set students(newStudents) {
-        if (!Array.isArray(newStudents)) {
-            throw new TypeError('Students must be an array');
-        }
-        this._students = newStudents;
+  set students(newStudents) {
+    if (!Array.isArray(newStudents)) {
+      throw new TypeError('Students must be an array');
     }
+    this._students = newStudents;
+  }
 }
--- ES6_classes/7-airport.js
+++ ES6_classes/7-airport.js
@@ -1,12 +1,12 @@
 // Implement a class named Airport
 
 export default class Aireport {
-    constructor(name, code) {
-        this._name = name
-        this._code = code
-    }
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
 
-    get [Symbol.toStringTag]() {
-        return `${this._code}`;
-    };
+  get [Symbol.toStringTag]() {
+    return `${this._code}`;
+  }
 }
