1505ca030f3f97e9b2a739cd24681680de705122
micaelapicco <micaelaabril421@gmail.com>
2023-09-18 11:23:18 -0700

    task 10

---
+++ ES6_classes/10-car.js
@@ -0,0 +1,17 @@
+// Task 10: Implement a class named Car
+
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  static get [Symbol.species]() {
+    return this;
+  }
+
+  cloneCar() {
+    return new this.constructor[Symbol.species]();
+  }
+}
