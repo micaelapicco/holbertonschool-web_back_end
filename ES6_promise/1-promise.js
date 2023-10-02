35a197f23bc6680cba13be93f9aadeec55b95ceb
micaelapicco <micaelaabril421@gmail.com>
2023-10-02 10:17:52 -0700

    first version

---
+++ ES6_promise/1-promise.js
@@ -0,0 +1,15 @@
+function getFullResponseFromAPI(success) {
+  return new Promise(((resolve, reject) => {
+    if (success === true) {
+      const response = {
+        status: 200,
+        body: 'Success',
+      };
+      resolve(response);
+    } else {
+      const error = new Error('The fake API is not working currently');
+      reject(error);
+    }
+  }));
+}
+export default getFullResponseFromAPI;
