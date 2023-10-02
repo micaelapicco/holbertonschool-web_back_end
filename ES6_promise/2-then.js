76757913e8411d486faab76bfaff0810fbed5e96
micaelapicco <micaelaabril421@gmail.com>
2023-10-02 10:45:49 -0700

    second version task 2

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -2,15 +2,18 @@ function handleResponseFromAPI(promise) {
   return promise
     /*eslint-disable*/
     .then((data) => {
+    /* eslint-enable */
       console.log('Got a response from the API');
       return {
-        stauts: 200,
         body: 'success',
+        status: 200,
       };
     })
+    /*eslint-disable*/
     .catch((error) => {
+    /* eslint-enable */
       console.log('Got a response from the API');
-      return new Error();
+      throw new Error();
     });
 }
 export default handleResponseFromAPI;
