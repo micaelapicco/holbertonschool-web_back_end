91b18047dbfd78e0438f28de1a300041a22b5a05
micaelapicco <micaelaabril421@gmail.com>
2023-10-12 07:44:54 -0700

    task 2 first version

---
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,4 @@
+export default function getStudentsByLocation(getListStudents, city) {
+  if (!Array.isArray(getListStudents)) return [];
+  return getListStudents.filter((student) => student.location === city);
+}
