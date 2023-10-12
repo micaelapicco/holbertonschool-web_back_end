5e7d6cf1e39e9d94e137b6e90f0d51e859d7d877
micaelapicco <micaelaabril421@gmail.com>
2023-10-12 06:22:45 -0700

    task 6 fix eslint

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -4,12 +4,12 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, fileName) {
   const user = signUpUser(firstName, lastName);
   const photo = uploadPhoto(fileName);
-  
+
   return Promise.allSettled([user, photo])
-  .then((results) => results.map((result, index) => (
-    {
-      status: result.status,
-      value: index === 0 ? result.value : result.reason.toString(),
-    }
-  )));
+    .then((results) => results.map((result, index) => (
+      {
+        status: result.status,
+        value: index === 0 ? result.value : result.reason.toString(),
+      }
+    )));
 }
